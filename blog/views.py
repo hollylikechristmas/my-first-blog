32b10996d028430271c7b9c8b17a4c4344b9698e
hollylikechristmas <hollymariegualco@gmail.com>
2017-05-13 14:27:54 -0700

    Correctly adding PostForm into import info.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 
